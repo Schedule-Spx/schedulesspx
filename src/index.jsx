618e5961945093fe58fe0c9837e1a36c57dd1402
david <david@davids-MacBook-Pro.local>
2024-08-29 12:43:57 -0400

    Dynamically apply theme background color across the site

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
-import { ThemeProvider } from './ThemeContext';
+import { ThemeProvider, useTheme } from './ThemeContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const Root = () => {
+  const { currentTheme } = useTheme();
+
+  return (
+    <div 
+      className="min-h-screen" 
+      style={{ backgroundColor: currentTheme.main }} // Apply the theme's main background color
+    >
+      <App />
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <ThemeProvider>
-        <div className="main-background min-h-screen">
-          <App />
-        </div>
+        <Root />
       </ThemeProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>,
