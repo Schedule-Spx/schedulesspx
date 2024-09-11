30db27844ce3dd62ed9be59615aab36908f53780
Kagen Jensen <kdogdevs@kagens-macbook-pro.tail2aaa6.ts.net>
2024-09-11 08:40:30 -0400

    Your commit message here

--- src/pages/MainDashboard.jsx
+++ src/pages/MainDashboard.jsx
@@ -20,7 +20,7 @@ const MainDashboard = () => {
   // Original heights reduced by 10%
   const originalHeights = {
     scheduleHeight: 390,
-    googleCalendarHeight: 375,
+    googleCalendarHeight: 372,
     dayHeaderHeight: 163,
     quickLinksHeight: 300,
     googleSuiteLinksHeight: 163,
